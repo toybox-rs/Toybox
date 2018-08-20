25a1a4594faadd8f9beca0696f9092b5ac1bc04a
John Foley <jfoley@cs.umass.edu>
2018-08-20 09:21:43 -0400

    vec2d type

--- toybox/src/lib.rs
+++ toybox/src/lib.rs
@@ -1,4 +1,21 @@
 
+#[derive(Debug,PartialEq,Eq)]
+struct Vec2D {
+    x: i32,
+    y: i32,
+}
+
+impl Vec2D {
+    fn new(x: i32, y: i32) -> Vec2D {
+        Vec2D { x, y }
+    }
+    fn origin() -> Vec2D {
+        Vec2D { x: 0, y: 0 }
+    }
+    fn translate(&self, by: Vec2D) -> Vec2D {
+        Vec2D::new(self.x + by.x, self.y + by.y)
+    }
+}
 
 enum Drawable {
     Rectangle(u32, u32, u32, u32),
@@ -6,13 +23,19 @@ enum Drawable {
 }
 
 trait GameVisualOutput {
-    fn display_list() -> &[Drawable];
+    fn game_size() -> Vec2D;
+    fn display_list() -> Vec<Drawable>;
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut ball = Vec2D::new(7,3);
+        let velocity = Vec2D::new(4,4);
+        let pos = ball.translate(velocity);
+        assert_eq!(pos, Vec2D::new(11,7));
     }
 }
