c64f68d429e0c29990032e471386f1b1a9f57125
John Foley <jfoley@cs.umass.edu>
2018-08-20 11:09:49 -0400

    Bouncing ball and Breakout state

--- toybox/src/bin/breakout.rs
+++ toybox/src/bin/breakout.rs
@@ -1,26 +1,163 @@
 extern crate toybox;
 
-use toybox::Vec2D;
+mod breakout {
+    use toybox::Vec2D;
+    use toybox::Body2D;
 
-const GameSize: (i32,i32) = (160, 160);
+    pub const GameSize: (i32,i32) = (240, 240);
 
-struct BreakoutState {
-    ball_pos: Vec2D,
-    ball_velocity: Vec2D,
-    paddle_pos: Vec2D,
-}
+    #[derive(Debug, Clone)]
+    pub struct Brick {
+        /// Brick position describes the upper-left of the brick.
+        pub position: Vec2D,
+        /// Brick size is the width and height of the brick.
+        pub size: Vec2D,
+        /// This is the number of points for a brick.
+        pub points: u32,
+        // todo, color?
+    }
 
-impl BreakoutState {
-    fn new() -> BreakoutState {
-        let seed = [13 as u8; 16];
-        BreakoutState { 
-            ball_pos: Vec2D::origin(), 
-            ball_velocity: Vec2D::from_polar(4.0, 0.5),
-            paddle_pos: Vec2D::origin() 
+    impl Brick {
+        pub fn new(position: Vec2D, size: Vec2D, points: u32) -> Brick {
+            Brick { position, size, points }
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct BreakoutState {
+        /// ball position describes the center of the ball.
+        pub ball: Body2D,
+        pub ball_radius: f64,
+        /// paddle position describes the center of the paddle.
+        pub paddle: Body2D,
+        pub paddle_width: f64,
+        pub bricks: Vec<Brick>,
+    }
+
+    impl BreakoutState {
+        /// Create a new game of breakout.
+        pub fn new() -> BreakoutState {
+            let (w,h) = GameSize;
+            let mut bricks = Vec::new();
+            let mut ball = Body2D::new_pos((w as f64) / 2.0, (h as f64) / 2.0);
+            ball.velocity = Vec2D::from_polar(4.0, 0.5);
+
+            let num_bricks_across = 12;
+            let num_bricks_deep = 5;
+            let xs = (w as f64) / (num_bricks_across as f64);
+            let ys = 12.0;
+            let bsize = Vec2D::new(xs, ys);
+            for x in 0..num_bricks_across {
+                for y in 0..num_bricks_deep {
+                    let bpos = Vec2D::new(x as f64 * xs, y as f64 * ys) ;
+                    bricks.push(Brick::new(bpos, bsize.clone(), y+1));
+                }
+            }
+
+            BreakoutState { 
+                ball: ball,
+                ball_radius: 4.0,
+                paddle: Body2D::new_pos((w as f64) / 2.0, (h as f64) - 30.0),
+                paddle_width: 32.0,
+                bricks
+            }
+        }
+        /// Mutably update the game state with a given time-step.
+        pub fn update_mut(&mut self, time_step: f64) {
+            // Update positions.
+            self.ball.integrate_mut(time_step);
+            self.paddle.integrate_mut(time_step);
+
+            let game_width = GameSize.0 as f64;
+            let game_height = GameSize.1 as f64;
+            
+            // Handle collisions:
+            if self.ball.velocity.y > 0.0 {
+                // ball is "falling", check floor, paddle, bricks
+
+                // check lose?
+                if (self.ball.position.y + self.ball_radius > game_height) {
+                    // TODO, lose
+                    self.ball.velocity.y *= -1.0;
+                }
+
+            } else {
+                // ball is "rising", check ceiling, bricks
+
+                // bounce ceiling?
+                if (self.ball.position.y - self.ball_radius < 0.0) {
+                    self.ball.velocity.y *= -1.0;
+                }
+            }
+
+            if (self.ball.velocity.x > 0.0) {
+                // bounce right wall?
+                if (self.ball.position.x + self.ball_radius > game_width) {
+                    self.ball.velocity.x *= -1.0;
+                }
+            } else {
+                // bounce left wall?
+                if (self.ball.position.x - self.ball_radius < 0.0) {
+                    self.ball.velocity.x *= -1.0;
+                }
+
+            }
         }
     }
 }
 
-fn main() {
+extern crate quicksilver;
+use quicksilver::{
+    State, run,
+    geom::{Rectangle, Circle},
+    graphics::{Color,Draw,Window,WindowBuilder}
+};
+use breakout::*;
 
+struct BreakoutGame {
+    state: BreakoutState,
+}
+
+fn score_to_color(score: u32) -> Color {
+    match score {
+        1 => Color::red(),
+        2 => Color::orange(),
+        3 => Color::yellow(),
+        4 => Color::green(),
+        5 => Color::blue(),
+        _ => Color::white()
+    }
+}
+
+impl State for BreakoutGame {
+    fn new() -> BreakoutGame {
+        BreakoutGame { state: BreakoutState::new() }
+    }
+    fn draw(&mut self, window: &mut Window) {
+        window.clear(Color::black());
+        for brick in &self.state.bricks {
+            let (x,y) = brick.position.pixels();
+            let (w,h) = brick.size.pixels();
+            window.draw(&Draw::rectangle(Rectangle::new(x,y,w-1,h-1))
+                            .with_color(score_to_color(brick.points)));
+        }
+
+        let (paddle_x, paddle_y) = self.state.paddle.position.pixels();
+        let paddle_w = self.state.paddle_width as i32;
+
+        window.draw(&Draw::rectangle(Rectangle::new(paddle_x - paddle_w/2, paddle_y, paddle_w, 10))
+                        .with_color(Color::indigo()));
+        let (ball_x, ball_y) = self.state.ball.position.pixels();
+        let ball_r = self.state.ball_radius as i32;
+        window.draw(&Draw::circle(Circle::new(ball_x, ball_y, ball_r))
+                        .with_color(Color::white()));
+
+        self.state.update_mut(1.0);
+
+        window.present();
+    }
+}
+fn main() {
+    let (w,h) = GameSize;
+    run::<BreakoutGame>(WindowBuilder::new("Breakout", w as u32, h as u32));
 }
--- toybox/src/lib.rs
+++ toybox/src/lib.rs
@@ -1,49 +1,30 @@
 use std::f64;
 
-/// This represents a point or a size or a velocity in 2 dimensions.
-/// We use f64 for internal representations but we can get integer coordinates upon request for drawing.
-#[derive(Debug,PartialEq)]
-pub struct Vec2D {
-    x: f64,
-    y: f64,
-}
+mod vec2d;
+pub use vec2d::Vec2D;
 
-impl Vec2D {
-    /// Create a new vector at the origin.
-    pub fn origin() -> Vec2D {
-        Vec2D { x: 0.0, y: 0.0 }
-    }
-    /// Create a new vector from components.
-    pub fn new(x: f64, y: f64) -> Vec2D {
-        Vec2D { x, y }
-    }
-    /// Create a new vector from angle and speed.
-    pub fn from_polar(r: f64, theta: f64) -> Vec2D {
-        Vec2D::new(r * theta.cos(), r * theta.sin())
-    }
-
-    pub fn magnitude(&self) -> f64 {
-        (self.x*self.x + self.y*self.y).sqrt()
-    }
-
-    /// Get the angle of this vector in radians.
-    pub fn angle(&self) -> f64 {
-        self.y.atan2(self.x)
-    }
-
-    pub fn translate(&self, by: &Vec2D) -> Vec2D {
-        Vec2D::new(self.x + by.x, self.y + by.y)
-    }
-    /// In order to render, we want pixel coordinates.
-    pub fn pixels(&self) -> (i32, i32) {
-        (self.x.floor() as i32, self.y.floor() as i32)
-    }
-}
-
+#[derive(Debug,Clone)]
 pub struct Body2D {
-    pos: Vec2D,
-    vel: Vec2D,
-    accel: Vec2D,
+    pub position: Vec2D,
+    pub velocity: Vec2D,
+    pub acceleration: Vec2D,
+}
+
+impl Body2D {
+    pub fn new_pos(x: f64, y: f64) -> Body2D {
+        Body2D::new_detailed(x,y,0.0,0.0,0.0,0.0)
+    }
+    pub fn new_detailed(x: f64, y: f64, vx: f64, vy: f64, ax: f64, ay: f64) -> Body2D { 
+        Body2D {
+            position: Vec2D::new(x,y), 
+            velocity: Vec2D::new(vx, vy), 
+            acceleration: Vec2D::new(ax, ay) 
+        }
+    }
+    pub fn integrate_mut(&mut self, time_step: f64) {
+        self.position += self.velocity.scale(time_step);
+        self.velocity += self.acceleration.scale(time_step);
+    }
 }
 
 enum Drawable {
@@ -54,18 +35,4 @@ enum Drawable {
 trait GameVisualOutput {
     fn game_size() -> Vec2D;
     fn display_list() -> Vec<Drawable>;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let mut ball = Vec2D::new(7.0,3.0);
-        let velocity = Vec2D::new(4.0,4.0);
-        let pos = ball.translate(&velocity);
-        assert_eq!(pos, Vec2D::new(11.0,7.0));
-        assert_eq!(velocity.magnitude(), (32.0 as f64).sqrt())
-    }
 }
