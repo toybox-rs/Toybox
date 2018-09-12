3425aaff91a3113d20101bfc1068688a0553f1eb
Emma Tosch <etosch@cs.umass.edu>
2018-09-12 17:22:56 -0400

    segfaults everywhere

--- openai/openai_shim/openai_shim/toybox.py
+++ openai/openai_shim/openai_shim/toybox.py
@@ -1,22 +1,93 @@
 import ctypes
+import numpy as np
 
 _lib_path = "target/debug/libopenai.dylib"
 _lib = ctypes.CDLL(_lib_path)
 
-def _get_game(game_name):
-    _lib.new_game.argtypes = [ctypes.c_char_p]
-    _lib.new_game.restype = ctypes.c_void_p
-    return _lib.new_game(game_name)
+_lib.alloc_game_simulator.argtypes = [ctypes.c_char_p]
+_lib.alloc_game_simulator.restype = ctypes.c_void_p
 
-class State(ctypes.Structure):
-    _fields_ = []
+_lib.frame_width.argtypes = [ctypes.c_void_p]
+_lib.frame_width.restype = ctypes.c_int8
+
+_lib.frame_height.argtypes = [ctypes.c_void_p]
+_lib.frame_height.restype = ctypes.c_int8
+
+
+def _get_frame(game):
+    _lib.get_frame.argtypes = [ctypes.c_void_p]
+    # _lib.get_frame.restype = [ctypes.c_]
+    
+
+def _to_action(action):
+    pass
+
+def _apply_action(game, action):
+    pass
+
+def _get_score(game):
+    pass
+
+
+class Simulator(object):
+    def __init__(self, game_name):
+        as_utf8 = game_name.encode('utf-8')
+        cstring_game_name = ctypes.create_string_buffer(as_utf8)
+        sim = _lib.alloc_game_simulator(cstring_game_name)
+        # sim should be a pointer
+        self.__sim = sim
+        self.__width = _lib.frame_width(self.__sim)
+        # self.__height = _lib.frame_width(self.__sim)
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        _lib.free_game_simulator.argtypes = [ctypes.c_void_p]
+        _lib.free_game_simulator(self.__sim)
+        self.__sim = None
+
+    def get_board_width(self):
+        return self.__width
+
+    def get_board_height(self):
+        return self.__height
+
+
+class State(object):
+    def __init__(self, sim):
+        _lib.alloc_game_state.argtypes = [ctypes.c_void_p]
+        _lib.alloc_game_state.restype = ctypes.c_void_p
+        self.__state = _lib.alloc_game_state(sim.__sim)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        _lib.free_game_state.argtypes = [ctypes.c_void_p]
+        _lib.free_game_state(self.__state)
+        self.__state = None
 
 class Toybox():
 
     def __init__(self, game_name):
-        self.game = _get_game(ctypes.create_string_buffer(game_name.encode('utf-8')))
+        self.game = _get_game()
+        # OpenAI state is a 4-frame sequence
+        self.state = tuple([_get_frame(self.game)] * 4)
+
+    def get_state(self):
+        return self.state
+
+    def apply_action(self, action):
+        action = _to_action(action)
+        new_frame = _apply_action(self.game, action)
+        self.state = (self.state[1], self.state[2], self.state[3], new_frame)
+        return new_frame
+
+    def get_score(self):
+        return _get_score(self.game)
 
 
 if __name__ == "__main__":
-    tb = Toybox("breakout")
-    print("Toybox", tb, tb.game)
+    with Simulator("breakout") as sim:
+        print(sim)
--- openai/src/lib.rs
+++ openai/src/lib.rs
@@ -2,20 +2,76 @@
 
 extern crate failure;
 extern crate toybox;
-use toybox::State;
+use toybox::{Simulation, State};
+use toybox::graphics::{render_to_buffer, ImageBuffer};
 use std::boxed::Box;
 use std::ffi::CStr;
 
+// SIMULATOR ALLOC + FREE
 #[no_mangle]
-pub extern "C" fn new_game(name: *const i8) -> *mut State {
+pub extern "C" fn alloc_game_simulator(name: *const i8) -> *mut Simulation {
     let name : &CStr = unsafe { CStr::from_ptr(name) };
     let name : &str = name.to_str().expect("poop!");
     print!("game: {}", name);
-    let state = toybox::get_simulation_by_name(name)
-        .unwrap()
-        .new_game();
-    Box::into_raw(state)
+    let simulation = toybox::get_simulation_by_name(name).unwrap();
+    Box::into_raw(simulation)
 }
+
+#[no_mangle]
+pub extern "C" fn free_game_simulator(simulator: *mut Simulation) {
+    if simulator.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(simulator);
+    }
+}
+
+// STATE ALLOC + FREE
+#[no_mangle]
+pub extern "C" fn alloc_game_state(simulator: &mut Simulation) -> *mut State {
+    Box::into_raw(simulator.new_game())
+}
+
+pub extern "C" fn free_game_state(state: *mut State) {
+    if state.is_null() {
+        return;
+    }
+    unsafe {
+        Box::from_raw(state);
+    }
+}
+
+// Need this information to initialize the numpy array in python
+#[no_mangle]
+pub extern "C" fn frame_width(simulator: *mut Simulation) -> i32 {
+    let simulator = unsafe { Box::from_raw(simulator) };
+    let (w, _) = simulator.game_size();
+    w
+}
+
+#[no_mangle]
+pub extern "C" fn frame_height(simulator: &mut Simulation) -> i32 {
+    let (_, h) = simulator.game_size();
+    h
+}
+
+
+#[no_mangle]
+pub extern "C" fn render_current_frame(numpy_pixels: &mut [u8], numpy_pixels_len : i32, simulator: &mut Simulation, state: &mut State) {
+    let (w, h) = simulator.game_size();
+    let mut img = ImageBuffer::alloc(w, h);
+    render_to_buffer(&mut img, &state.draw());
+
+    for i in 0..w {
+        for j in 0..h {
+            let k = (j * w + i) as usize;
+            numpy_pixels[k] = img.data[k];
+        }
+    }
+}
+
+// void render_current_frame(void* numpy_pixels, size_t numpy_pixels_len, void* game_state, void* simulator);
 
 // fn simulate_n_frames(game_state: &mut State, n: u32) {
 //   for _ in range(n) {
@@ -31,13 +87,4 @@ pub extern "C" fn new_game(name: *const i8) -> *mut State {
 // } 
 
 
-// // unsafe { Box::into_raw(simulator_by_name(...)) and std::mem::forget } (written in Rust)
-// void* alloc_game_simulator(const char* which_one);
-// // unsafe { let _ = Box::from_raw(which_one) }, drop takes care of freeing.
-// void free_game_simulator(void* simulator);
-// int simulator_width(void* simulator);
-// int simulator_height(void* simulator);
-// void* alloc_new_game_state(void* simulator);
-// void free_game_state(void* state);
 // void simulate_n_frames(void* game_state, unsigned int n);
-// void render_current_frame(void* numpy_pixels, size_t numpy_pixels_len, void* game_state, void* simulator);
