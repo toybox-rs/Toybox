cffcf22c212ebbb8cbcf75f003f26a307d37ffd6
Emma Tosch <etosch@cs.umass.edu>
2018-09-17 14:59:46 -0400

    set up try_amidar.sh as an sbatch job

--- openai/try_amidar.sh
+++ openai/try_amidar.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --job-name=try-amidar
+#SBATCH --output=foo_%j.out
+#SBATCH -e foo_%j.err
 NUM_FRAMES=$1
 
-cargo build --release
-
-./start_python toybox_baselines.py --alg=ppo2 --env=toybox-amidar-v0 --num_timesteps=$1 --save_path=$PWD/amidar.ppo2.$1.model
+LD_LIBRARY_PATH=gypsum:/home/etosch/toybox/openai/target/release ./start_python toybox_baselines.py --alg=ppo2 --env=toybox-amidar-v0 --num_timesteps=$1 --save_path=$PWD/amidar.ppo2.$1.model
