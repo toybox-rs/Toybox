ce2242b37085f5a1da75cb2e72cc658c352b3bce
kclary <kclary@cs.umass.edu>
2018-09-26 17:27:51 -0400

    compute all the things

--- openai/baselines/baselines/run.py
+++ openai/baselines/baselines/run.py
@@ -242,7 +242,7 @@ def main():
             num_lives = turtle.ale.lives()
             obs, _, done, info = env.step(actions)
             #done = done and (num_lives == 1 or turtle.ale.game_over())
-            env.render()
+            #env.render()
             time.sleep(1.0/60.0)
             done = num_lives == 1 and done 
             #done = done.any() if isinstance(done, np.ndarray) else done
@@ -266,7 +266,9 @@ def main():
 
 
         print("Avg score: %f" % np.average(scores))
-        print("Std. score: %f" % sem(scores))
+        print("Median score: %f" % np.median(scores))
+        print("Std error score: %f" % sem(scores))
+        print("Std dev score: %f" % statistics.stdev(scores)
         env.close()
 
 if __name__ == '__main__':
