020a6a62115a3c2e2f26a7797321dc2426eb6a7b
John Foley <jfoley@cs.umass.edu>
2018-11-09 11:52:37 -0500

    Working toward canvas output #49

--- breakout-web-play/www/index.js
+++ breakout-web-play/www/index.js
@@ -1,3 +1,14 @@
 import * as wasm from "breakout-web-play";
 
+var canvas = document.getElementById("breakout");
+var ctx = canvas.getContext("2d");
+
 wasm.greet();
+
+function step() {
+  console.log("step");
+  window.requestAnimationFrame(step);
+}
+
+step();
+
