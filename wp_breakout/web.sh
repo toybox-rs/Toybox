a72cc3e8f29da699269eb238443cc475fb675376
John Foley <jfoley@cs.umass.edu>
2018-12-24 11:28:57 -0500

    fix web paths

--- wp_breakout/web.sh
+++ wp_breakout/web.sh
@@ -5,7 +5,7 @@ set -eu
 # Derived from: https://rustwasm.github.io/wasm-bindgen/whirlwind-tour/basic-usage.html
 cargo build --target=wasm32-unknown-unknown --release
 mkdir -p gen
-wasm-bindgen ../target/wasm32-unknown-unknown/release/toybox_wasm.wasm \
+wasm-bindgen ../target/wasm32-unknown-unknown/release/toybox_wp_breakout.wasm \
   --out-dir gen
 
 rm -rf target
