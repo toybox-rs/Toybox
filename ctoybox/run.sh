14a9824a92bd052808fd94336a39dc2ca1a59de3
Emma Tosch <etosch@cs.umass.edu>
2018-12-29 11:11:38 -0500

    added training and testing seeds

--- ctoybox/run.sh
+++ ctoybox/run.sh
@@ -1,6 +1,4 @@
-envs="toybox-amidar-v0 AmidarNoFrameskip-v0 toybox-breakout-v0 BreakoutNoFrameskip-v0"
 algs="acer acktr a2c ppo2 deepq"
-timesteps="1e7 3e7 5e7"
 work1=/mnt/nfs/work1/jensen/etosch/issta
 logs=$work1/logs
 
@@ -9,10 +7,10 @@ mkdir -p $logs
 partition="titanx-long"
 
 
-envs="BreakoutToyboxNoFrameskip-v4"
-algs="ppo2"
-timesteps="1e7"
-weights="[1.0,0.0] [0.5,0.5] [0.0,1.0]"
+envs="BreakoutNoFrameskip-v4 AmidarNoFrameskip-v4 SpaceInvadersNoFrameskip-v4"
+timesteps="1e7 5e7"
+weights=0
+seeds=`cat training_seeds`
 
 
 # make sure we have all the pip dependencies we want installed
@@ -23,26 +21,28 @@ chmod +x install_rust.sh
 ./install_rust.sh -y
 source $HOME/.cargo/env
 rustup default stable
-cargo build --release 
+cargo build --release
 
 for env in $envs; do
-    for alg in $algs; do 
-	for steps in $timesteps; do
-	    for weight in $weights; do
-		if [ -z $weight ]; then 
-		    uid=$env.$alg.$steps
-		else 
-		    wname=`python3 -c "print('$weight'.replace('[','').replace(']','').replace(',','').replace('.',''))"`		    
-		    uid=$env.$alg.$steps.$wname
-		fi
-		model=$work1/$uid.`date -I`.model
-		dest=run_cmd_$uid.sh
-		logdir=$logs/$uid
+    for seed in $seeds; do 
+        for alg in $algs; do 
+	    for steps in $timesteps; do
+	        for weight in $weights; do
+		    if [ "$weights" -eq "0" ]; then 
+		        uid=$env.$alg.$steps.$seed
+		    else 
+		        wname=`python3 -c "print('$weight'.replace('[','').replace(']','').replace(',','').replace('.',''))"`		    
+		        uid=$env.$alg.$steps.$seed.$wname
+		    fi
+                    echo "Processing model $uid"
+		    model=$work1/$uid.`date -I`.model
+		    dest=run_cmd_$uid.sh
+		    logdir=$logs/$uid
 
-		mkdir -p $logdir
-		
-		echo "Running on $partition. Command saved to $dest."
-		
+		    mkdir -p $logdir
+
+		    echo "Running on $partition. Command saved to $dest."
+
 		cmd="#!/bin/bash
 #
 #SBATCH --job-name=$uid
@@ -50,11 +50,14 @@ for env in $envs; do
 #SBATCH -e $uid.err
 #SBATCH --mem=16g
 
-OPENAI_LOGDIR=$logdir OPENAI_FORMAT=csv ./start_python -m baselines.run --alg=$alg --env=$env --num_timesteps=$steps --save_path=$model --weights=$weight" 
+OPENAI_LOGDIR=$logdir OPENAI_FORMAT=csv ./start_python -m baselines.run --alg=$alg --seed=$seed --env=$env --num_timesteps=$steps --save_path=$model --weights=$weight" 
 	    echo "$cmd"
 	    echo "$cmd" > $dest
-	    sbatch -p $partition --gres=gpu:1 $dest
+
+            
+            sbatch -p $partition --gres=gpu:1 $dest
+	        done;
 	    done;
-	done;
+        done;
     done;
 done
