5fb9edd6888b0855bd47ee195bcba8a9f1dea74f
kclary <kclary@cs.umass.edu>
2019-03-01 16:28:11 -0500

    complete intervention functions for breakout and add some tests in main

--- ctoybox/toybox/toybox/interventions/amidar.py
+++ ctoybox/toybox/toybox/interventions/amidar.py
@@ -1,40 +1,34 @@
-def amidar_num_tiles_unpainted(self):
-        assert(self.game_name == 'amidar')
+# Methods on amidar intervention
+
+class AmidarIntervention(Intervention):
+
+    def amidar_num_tiles_unpainted(self):
         return self.query_json('num_tiles_unpainted')
-    
-def amidar_player_tile(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('player_tile')
+        
+    def amidar_player_tile(self):
+        return self.query_json('player_tile')
 
-def amidar_num_enemies(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('num_enemies')
+    def amidar_num_enemies(self):
+        return self.query_json('num_enemies')
 
-def amidar_jumps_remaining(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('jumps_remaining')
+    def amidar_jumps_remaining(self):
+        return self.query_json('jumps_remaining')
 
-def amidar_regular_mode(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('regular_mode')
+    def amidar_regular_mode(self):
+        return self.query_json('regular_mode')
 
-def amidar_jump_mode(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('jump_mode')
+    def amidar_jump_mode(self):
+        return self.query_json('jump_mode')
 
-def amidar_chase_mode(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('chase_mode')
+    def amidar_chase_mode(self):
+        return self.query_json('chase_mode')
 
-def amidar_enemy_tiles(self):
-    assert(self.game_name == 'amidar')
-    return self.query_json('enemy_tiles')
+    def amidar_enemy_tiles(self):
+        return self.query_json('enemy_tiles')
 
-def amidar_enemy_caught(self, eid):
-    assert(self.game_name == 'amidar')
-    return self.query_json('enemy_caught', eid)
+    def amidar_enemy_caught(self, eid):
+        return self.query_json('enemy_caught', eid)
 
-def amidar_any_enemy_caught(self, eid):
-    assert(self.game_name == 'amidar')
-    num_enemies = self.amidar_num_enemies()
-    return any(self.amidar_enemy_caught(eid) for eid in range(num_enemies))
+    def amidar_any_enemy_caught(self, eid):
+        num_enemies = self.amidar_num_enemies()
+        return any(self.amidar_enemy_caught(eid) for eid in range(num_enemies))
--- ctoybox/toybox/toybox/interventions/breakout.py
+++ ctoybox/toybox/toybox/interventions/breakout.py
@@ -1,25 +1,21 @@
+from toybox.interventions.base import *
+import json
 """An API for interventions on breakout."""
 
-class BreakoutInterventions(Intervention):
+class BreakoutIntervention(Intervention):
 
-    def breakout_brick_live_by_index(self, index):
-        return self.tb.rstate.query_json('brick_live_by_index', json.dumps(index))
+    def __init__(self, tb, game_name='breakout'):
+        # check that the simulation in tb matches the game name.
+        Intervention.__init__(self, tb, game_name)
 
-    def breakout_bricks_remaining(self):
-        return self.tb.rstate.query_json('bricks_remaining')
+    def num_bricks_remaining(self):
+        return sum(self.state['bricks'][i]['alive'] for i in range(self.num_bricks()))
     
-    def breakout_channel_count(self):
-        return self.tb.rstate.query_json('count_channels')
+
+    def channel_count(self):
+        stacks = self.get_stacks()
+        return sum(self.is_channel(stack) for stack in stacks)
     
-    def breakout_num_columns(self):
-        return self.tb.rstate.query_json('num_columns')
-
-    def breakout_num_rows(self):
-        return self.tb.rstate.query_json('num_rows')
-
-    def breakout_channels(self):
-        return self.tb.rstate.query_json('channels')
-
 
     def get_bricks(self):
         return self.state['bricks']
@@ -49,15 +45,15 @@ class BreakoutInterventions(Intervention):
 
 
     def num_columns(self):
-        rows = num_rows(_toybox)
-        bricks = num_bricks(_toybox)
+        rows = self.num_rows()
+        bricks = self.num_bricks()
         return bricks // rows
 
 
     def get_stacks(self):
         """Returns a list of lists. Each element returned is a list of indices, corresponding to a potential channel."""
-        ncols = num_columns(_toybox)
-        nrows = num_rows(_toybox)
+        ncols = self.num_columns()
+        nrows = self.num_rows()
         stacks = []
         for offset in range(ncols):
             stack = []
@@ -67,29 +63,28 @@ class BreakoutInterventions(Intervention):
         return stacks
 
 
-
     def is_channel(self, intlist):
-        bricks = get_bricks(_toybox)
-        return is_stack(intlist) and all([not is_alive(bricks[i]) for i in intlist])
+        bricks = self.get_bricks()
+        return self.is_stack(intlist) and all([not self.brick_alive(i) for i in intlist])
 
 
     def find_channel(self):
-        stacks = get_stacks(_toybox)
+        stacks = self.get_stacks()
         for i, stack in enumerate(stacks):
-            if is_channel(_toybox, stack):
+            if self.is_channel(stack):
                 return i, stack
         raise ValueError('No channels found.')
 
 
     def add_channel(self, intlist):
-        assert(is_stack(intlist))
+        assert(self.is_stack(intlist))
         for i in intlist:
-            remove_brick(_toybox, i)
+            self.set_brick(i, alive=False)
 
 
     def is_stack(self, intlist):
-        ncols = num_columns(_toybox)
-        nrows = num_rows(_toybox)
+        ncols = self.num_columns()
+        nrows = self.num_rows()
         offset = intlist[0] % ncols
         row = 1
         for i in intlist[1:]:
@@ -100,8 +95,6 @@ class BreakoutInterventions(Intervention):
 
 
 # Intervention tests
-    # remove and assert that the brick is gone
-    # reset and assert that the brick is present
     # assert number of bricks 
     # assert rows, columns, stacks
     # assert channel present, channel not present 
@@ -114,7 +107,56 @@ class BreakoutInterventions(Intervention):
     # paddle speed
     # all the constants!
 
+    # add brick in arbitrary location?
+
 
     def get_json(self):
-        assert(_toybox_json)
-        return _toybox_json
+        #assert(_toybox_json)  // why would we have asserted here? still necessary in the with statement?
+        return self.state
+
+
+if __name__ == "__main__":
+  with Toybox('breakout') as tb:
+    state = tb.to_state_json()
+    config = tb.config_to_json()
+
+    # save the 
+    with open('toybox/toybox/interventions/defaults/breakout_state_default.json', 'w') as outfile:
+        json.dump(state, outfile)
+
+    with open('toybox/toybox/interventions/defaults/breakout_config_default.json', 'w') as outfile:
+        json.dump(config, outfile)
+    
+    # remove and assert that the brick is gone
+    with BreakoutIntervention(tb) as intervention:
+        nbricks = intervention.num_bricks_remaining()
+        intervention.set_brick(0, alive=False)
+        nbricks_post = intervention.num_bricks_remaining()
+
+        assert nbricks - 1 == nbricks_post
+
+    # reset and assert that the brick is present
+    with BreakoutIntervention(tb) as intervention:
+        nbricks = intervention.num_bricks_remaining()
+        intervention.set_brick(0, alive=True)
+        nbricks_post = intervention.num_bricks_remaining()
+
+        assert nbricks + 1 == nbricks_post
+
+    # add a channel and assert that num_rows bricks have been removed
+    with BreakoutIntervention(tb) as intervention: 
+        nbricks = intervention.num_bricks_remaining()
+
+        stacks = intervention.get_stacks()
+        intervention.add_channel(stacks[0])
+        nbricks_post = intervention.num_bricks_remaining()
+        assert nbricks_post == nbricks - intervention.num_rows()
+
+        col, channel = intervention.find_channel()
+        assert channel
+
+    # remove a channel and assert that num_rows bricks have been added
+    with BreakoutIntervention(tb) as intervention: 
+        pass
+
+
