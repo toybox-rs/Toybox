fcecb5084151aa8e912a130472cd6f24addc33d8
kclary <kclary@cs.umass.edu>
2019-03-15 09:48:22 -0400

    added comments for planned functions

--- ctoybox/toybox/toybox/interventions/space_invaders.py
+++ ctoybox/toybox/toybox/interventions/space_invaders.py
@@ -13,9 +13,20 @@ class SpaceInvadersIntervention(Intervention):
         pass
 
 
+    # move player
+    # shift enemies +- x, +- y
+    # add/remove shields
+    # add/remove mothership 
+    # change ship speed
+    # make enemy/enemies take more than one hit
+    # get/set lives
+    # alter enemy_shot_delay
+    # custon enemy firing pattern
+
+
 if __name__ == "__main__":
   import argparse 
-  parser = argparse.ArgumentParser(description='test Amidar interventions')
+  parser = argparse.ArgumentParser(description='test Space Invaders interventions')
   parser.add_argument('--partial_config', type=str, default="null")
   parser.add_argument('--save_json', type=bool, default=False)
 
