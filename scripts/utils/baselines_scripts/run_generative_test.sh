b9873334fb6aec4740df8e85fe074d46806d95e9
kclary <kclary@cs.umass.edu>
2019-12-04 22:52:33 -0500

    update model input format; reorder loop so we can launch multiple xy tests per batch

--- scripts/utils/baselines_scripts/run_generative_test.sh
+++ scripts/utils/baselines_scripts/run_generative_test.sh
@@ -6,8 +6,7 @@ mkdir -p $logs
 
 partition="titanx-short"
 
-models=`cat neurips_models_amidar`
-models="AmidarNoFrameskip-v4.0505.a2c.1e8.2202933152.2019-05-13.10e7.model"
+models=`ls $work1/ijcai20/*.model`
 xys=`cat ../tb_amidar/src/resources/tileset`
 
 # make sure we have all the pip dependencies we want installed
@@ -23,39 +22,45 @@ source $HOME/.cargo/env
 rustup default stable
 cargo build --release
 
-mod_var=0
+njobs=-1
 
 for model in $models; do
 	minfo=$(echo $model | tr "." "\n")
 	minfo=($minfo)
 	env=${minfo[0]}
-        env="AmidarToyboxNoFrameskip-v4"
-        sampling=${minfo[1]}
-	alg=${minfo[2]}
-	steps=${minfo[3]}
-	seed=${minfo[4]}
-	dt=${minfo[5]}
+	alg=${minfo[1]}
+	steps=${minfo[2]}
+	seed=${minfo[3]}
+	mx=${minfo[4]}
+	my=${minfo[5]}
+	dt=${minfo[6]}
 
-    for xy_pair in $xys; do
-        mod_var=$((mod_var + 1))
-        echo $mod_var
-        if [ $(( $mod_var % 2 )) -eq 0 ]; then
+    echo "Processing model $uid"
+	uid=$env.$alg.$steps.$seed.$mx.$my
+	dest=run_cmd_$uid.sh
+    echo "Running on $partition. Command saved to $dest."
+
+    logdir=$logs/$uid
+    mkdir -p $logdir
+
+    cmd="#!/bin/bash
+#
+#SBATCH --job-name=$uid
+#SBATCH --output=$uid.out
+#SBATCH -e $uid.err
+#SBATCH --mem=16g"
+
+    njobs=$((njobs + 1))
+    echo $njobs
+    
+    if [ $njobs -ge 0 ] && [ $njobs -lt 120 ]; then
+        for xy_pair in $xys; do
             xy=$(echo $xy_pair | tr "," " ")
             xy=($xy)
             x=${xy[0]}
             y=${xy[1]} 
-    		
-            uid=$env.$alg.$steps.$seed.$x.$y
-            echo "Processing model $uid"
-
-            dest=run_cmd_$uid.sh
+            
             conf=trial_config_x_${x}_y_${y}.json
-
-            logdir=$logs/$uid
-            mkdir -p $logdir
-
-            echo "Running on $partition. Command saved to $dest."
-
             trial_config="{
 	\"player_start\": {
         \"tx\": 31,
@@ -75,21 +80,15 @@ for model in $models; do
     },
     \"_comment\": \"Amidar quick config for removing nonrandom noise from training. Modifications: unpaint initial segment to avoid implementation headches; no chase mode; no jump mode; set starting player position from set\"}"
                         
-             echo "$trial_config"
-             echo "$trial_config" > $conf
-
-
-             cmd="#!/bin/bash
-#
-#SBATCH --job-name=$uid
-#SBATCH --output=$uid.out
-#SBATCH -e $uid.err
-#SBATCH --mem=16g
-
+            echo "$trial_config"
+            echo "$trial_config" > $conf
+        done;
+        
+        cmd="$cmd
 OPENAI_LOGDIR=$logdir OPENAI_FORMAT=csv ./start_python_gypsum -m baselines.run --alg=$alg --env=$env --num_timesteps=0 --num_env=1 --load_path=$work1/$model --partial_config=$conf --play"
-	     echo "$cmd"
-             echo "$cmd" > $dest
-             sbatch -p $partition --gres=gpu:1 $dest
-         fi 
-    done;
+
+	    echo "$cmd"
+        echo "$cmd" > $dest
+        sbatch -p $partition --gres=gpu:1 $dest
+    fi
 done
