c88a0eb353df198a19187d0212093c10cad43ee5
Emma Tosch <etosch@cs.umass.edu>
2020-01-01 11:24:05 -0500

    commented out gridworld test

--- scripts/utils/unit_tests.sh
+++ scripts/utils/unit_tests.sh
@@ -5,6 +5,6 @@ set -eu
 ./start_python ../scripts/utils/test_games.py
 ./start_python toybox/toybox/interventions/amidar.py
 ./start_python toybox/toybox/interventions/breakout.py
-./start_python toybox/toybox/interventions/gridworld.py
+#./start_python toybox/toybox/interventions/gridworld.py
 ./start_python toybox/toybox/interventions/space_invaders.py
 
