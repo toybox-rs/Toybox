59c7a040559641bf749226ef6cedb02d9653e26c
Emma Tosch <etosch@cs.umass.edu>
2020-01-05 11:00:12 -0500

    missing args

--- ctoybox/toybox/toybox/sample_tests/base.py
+++ ctoybox/toybox/toybox/sample_tests/base.py
@@ -29,7 +29,7 @@ class ToyboxTestBase(behavior.BehavioralFixture):
         self.final_state = self.toybox.to_state_json()
       return self.done
 
-    def setUp(self): super().setUp(trials=2, timeout=500)
+    def setUp(self, **kwargs): super().setUp(trials=2, timeout=500)
 
 
 class AmidarToyboxTestBase(ToyboxTestBase):
