9e546fa0fe61d5069a26b20585a225dc87ffabab
John Foley <jjfoley@smith.edu>
2020-01-06 10:08:09 -0500

    not sure the python check ever worked correctly?

--- check.sh
+++ check.sh
@@ -3,15 +3,14 @@
 set -eu
 
 # just to make sure pip installs worked OK!
-python -c 'import ctoybox'
+./scripts/utils/unit_tests.sh 
 
+# regression / with models tests:
 if [ ! -e toybox-regress-models.zip ]; then
   wget https://static.jjfoley.me/toybox-regress-models-16-april-2019.zip -O toybox-regress-models.zip
   unzip toybox-regress-models.zip
 fi
 
-# required for gym env registration
-./scripts/utils/unit_tests.sh 
 ./scripts/utils/regress.sh
 ./scripts/utils/behavior_tests.sh
 
