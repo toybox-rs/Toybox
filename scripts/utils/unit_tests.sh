db3572ceb1577219ea44dec644b1fe055450b122
Kaleigh Clary <kclary@cs.umass.edu>
2021-03-24 17:10:20 -0400

    add back breakout, amidar unit tests

--- scripts/utils/unit_tests.sh
+++ scripts/utils/unit_tests.sh
@@ -3,8 +3,8 @@
 set -eu
 
 python scripts/utils/test_games.py
-#python -m toybox.interventions.amidar
-#python -m toybox.interventions.breakout
+python -m toybox.interventions.amidar
+python -m toybox.interventions.breakout
 python -m toybox.interventions.space_invaders
 
 python -m unittest discover test.interventions
